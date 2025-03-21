353527a06585904c917ae27fef2e894ea9049a02
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-03-21 11:17:58 +0100

    fix:updated the requiremnts and remove the secret key for prodiction

--- Auth_API/settings.py
+++ Auth_API/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o((jc2@cmwg2f+9g2*dab8qgg3ie%to!xt%w44hwy1lsmws5k0'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
